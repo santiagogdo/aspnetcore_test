a5c5977387ac38b0d1b05e3304fa42be2e06794c
Santiago <santiagogdo@gmail.com>
2018-09-17 02:02:13 -0300

    added files

--- aspnetcore_test/Startup.cs
+++ aspnetcore_test/Startup.cs
@@ -20,6 +20,10 @@ namespace aspnetcore_test
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
